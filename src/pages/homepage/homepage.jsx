0a7983a4174b0846b7b57b2347c9cc61b0232ce5
idris <idrisdoyin@gmail.com>
2021-06-08 22:54:02 +0100

    creat components and pages with transition

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory';
+
+import './homepage.scss';
+
+const Homepage = () => {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+};
+
+export default Homepage;
